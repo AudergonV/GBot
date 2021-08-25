91b5ae64e2466f208c00ee623605055aeea4e2e2
Audergon Vincent <vincent.audergon@bluewin.ch>
2021-08-25 23:43:25 +0200

    FIX

--- rank.js
+++ rank.js
@@ -1,8 +1,17 @@
 const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
 
+const randomRank = function () {
+    const list = [];
+    list.push("RANK?");
+    list.push("WHAT'S YOUR RANK?");
+    list.push("RANK RN");
+    list.push("RANK!!!");
+    list.push("COME ON, WHAT'S YOUR RANK?");
+    return list[Math.floor(Math.random() * list.length)];
+}
 
 const execute = async function(interaction){
-    await interaction.reply("RANK?");
+    await interaction.reply(randomRank());
 }
 
 module.exports = {
